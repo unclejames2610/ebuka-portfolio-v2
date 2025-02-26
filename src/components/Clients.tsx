4caeea98f3aacadb9190d5b3890b2e60122a33f5
unclejames2610 <udealaebuka@gmail.com>
2025-02-26 09:50:33 +0100

    fix:client error

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
 import { companies, testimonials } from "@/data";
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { workExperience } from "@/data";
 import React from "react";
 import { Button } from "./ui/MovingBorders";
