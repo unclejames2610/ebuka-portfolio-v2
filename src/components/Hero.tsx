2cbf731a083eb982e2518c439585ce177fcbb4d4
unclejames2610 <udealaebuka@gmail.com>
2025-04-10 15:39:43 +0100

    chore:color and hero text change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,16 +27,16 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+          <h2 className="uppercase tracking-widest text-xs text-center font-semibold text-blue-100 max-w-80">
+            Hey, I'm Ebuka
           </h2>
 
           <TextGenerateEffect
             className="text-center text-4xl md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Crafting beautiful and seamless user experiences as a Frontend Developer."
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Ebuka, a front-end web and mobile software developer.
+            Let's Build Something Amazing Together!
           </p>
 
           <Link href="#projects">
--- src/components/ui/TextGenerateEffect.tsx
+++ src/components/ui/TextGenerateEffect.tsx
@@ -38,7 +38,7 @@ export const TextGenerateEffect = ({
             <motion.span
               key={word + idx}
               className={`${
-                idx > 3 ? "text-purple" : "dark:text-white text-black"
+                idx > 7 ? "text-purple" : "dark:text-white text-black"
               }  opacity-0`}
               style={{
                 filter: filter ? "blur(10px)" : "none",
